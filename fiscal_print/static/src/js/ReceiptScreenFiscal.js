cc18a39d14ef209794483805fb290a9b8cf3fc75
efzeladat <48692120+efzeladat@users.noreply.github.com>
2021-04-10 14:23:32 -0400

    add validation to text field

--- fiscal_print/static/src/js/ReceiptScreenFiscal.js
+++ fiscal_print/static/src/js/ReceiptScreenFiscal.js
@@ -34,33 +34,29 @@ odoo.define('fiscal_print.ReceiptScreenFiscal', function (require) {
             const order = this.env.pos.get_order();
             const lines = order.orderlines.models;
             const payment = order.paymentlines.models;
-               var header = '<BOM>' +
-                 '<BO>' +
-                   '<AdmInfo>' +
-                     '<Object>13</Object>' +
-                     '<Version>2</Version>' +
-                   '</AdmInfo>' +
-                   '<Documents>' +
-                     '<row>' +
-                       '<DocEntry>' + order.uid.toString() + '</DocEntry>' +
-                       '<DocNum>' + order.uid.toString() + '</DocNum>' +
-                       '<DocType>Factura</DocType>' +
-                       '<DocDate>' + order.validation_date + '</DocDate>' +
-                       '<DocDueDate>' + order.date_order + '</DocDueDate>' +
-                       '<Address>' + (order.attributes.client.address) ? order.attributes.client.address : '' + '</Address>' +
-                       '<DocTotal>' + payment.amount.toString() + '</DocTotal>' +
-                       '<DocCurrency></DocCurrency>' +
-                       '<DocRate></DocRate>' +
-                       '<Reference1></Reference1>' +
-                       '<JournalMemo>Punto de Ventas</JournalMemo>' +
-                       '<DocTime>' + order.validation_date + '</DocTime>' +
-                       '<SalesPersonCode>' + order.pos.employee.name + '</SalesPersonCode>' +
-                       '<TaxDate>' + order.validation_date + '</TaxDate>' +
-                       '<FederalTaxID></FederalTaxID>' +
-                       '<DocTotalFc>' + payment.amount.toString() + '</DocTotalFc>' +
-                       '<Address2>' + (order.attributes.client.address) ? order.attributes.client.address : '' + '</Address2>' +
-                     '</row>' +
-                   '</Documents>';
+               var header = '<BOM><BO><AdmInfo><Object>13</Object><Version>2</Version></AdmInfo><Documents><row><DocEntry>';
+               header += (order.uid) ? order.uid.toString() : '';
+               header += '</DocEntry><DocNum>';
+               header += (order.uid) ? order.uid.toString() : '';
+               header += '</DocNum><DocType>Factura</DocType><DocDate>'
+               header += (order.validation_date) ? order.validation_date : '';
+               header += '</DocDate><DocDueDate>';
+               header += (order.validation_date) ? order.validation_date : '';
+               header += '</DocDueDate><Address>';
+               header += (order.attributes.client.address) ? order.attributes.client.address : '';
+               header += '</Address><DocTotal>'
+               header += (payment.amount) ? payment.amount.toString() : '';
+               header += '</DocTotal><DocCurrency></DocCurrency><DocRate></DocRate><Reference1></Reference1><JournalMemo>Punto de Ventas</JournalMemo><DocTime>';
+               header += (order.validation_date) ? order.validation_date : '';
+               header += '</DocTime><SalesPersonCode>';
+               header += (order.pos.employee.name) ? order.pos.employee.name : '';
+               header += '</SalesPersonCode><TaxDate>'
+               header += (order.validation_date) ? order.validation_date : '';
+               header += '</TaxDate><FederalTaxID></FederalTaxID><DocTotalFc>';
+               header += (payment.amount) ? payment.amount.toString() : '';
+               header += '</DocTotalFc><Address2>';
+               header += (order.attributes.client.address) ? order.attributes.client.address : '';
+               header += '</Address2></row></Documents>';
                var linesFile = '<Document_Lines>';
                for(var i=0;i < lines.length; i++) {
                    var line = lines[i];
@@ -91,17 +87,15 @@ odoo.define('fiscal_print.ReceiptScreenFiscal', function (require) {
                    }
                     linesFile += '</Document_Lines>';
 
-                   var tax = '<LineTaxJurisdictions>' +
-                     '<row>' +
-                       '<JurisdictionCode>' + order.uid.toString() + '</JurisdictionCode>' +
-                       '<JurisdictionType>1</JurisdictionType>' +
-                       '<TaxAmount>' + payment.amount.toString() + '</TaxAmount>' +
-                       '<DocEntry>' + order.uid.toString() + '</DocEntry>' +
-                       '<LineNumber>' + lines.length.toString() + '</LineNumber>' +
-                     '</row>' +
-                   '</LineTaxJurisdictions>' +
-                 '</BO>' +
-               '</BOM>';
+                   var tax = '<LineTaxJurisdictions><row><JurisdictionCode>';
+                   tax += (order.uid) ? order.uid.toString() : '';
+                   tax += '</JurisdictionCode><JurisdictionType>1</JurisdictionType><TaxAmount>';
+                   tax += (payment.amount) ? payment.amount.toString() : '';
+                   tax += '</TaxAmount><DocEntry>';
+                   tax += (order.uid) ? order.uid.toString() : '';
+                   tax += '</DocEntry><LineNumber>';
+                    tax += (lines.length) ? lines.length.toString() : '';
+                   tax += '</LineNumber></row></LineTaxJurisdictions></BO></BOM>';
                return header + linesFile + tax;
             }
         };
